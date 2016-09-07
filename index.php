03a07da77fa53f4c9f94162d2e24aa8f76851754
kof97 <1048434786@qq.com>
2016-09-07 10:37:48 +0800

    date

--- index.php
+++ index.php
@@ -2,13 +2,27 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <title>Arno-acfun每日排行</title>
+    <title>Arno-acfun 每日排行</title>
     <link rel="stylesheet" href="./dist/styles/style.min.css">
 </head>
 <body>
     <?php require './query.php'; ?>
-    
+
+	<div class="indexer shadow">
+       	<form action="" method="get">
+	    	<input type="text" name="time" onClick="WdatePicker({minDate:'2016-09-05',maxDate:'<?php echo $now; ?>'})" value="<?php echo $time; ?>"/>
+       		<input type="submit" value="submit">
+       	</form>
+    </div>
+
+	<div class="row"></div>
+	<div class="row"></div>
+
     <div class="main">
+		<?php if (count($categorys) < 1): ?>
+			<h2>没有找到 <?php echo $time; ?> 的内容，资源是从 2016-09-05 号才开始的哦 ~ ~ ~</h2>
+		<?php endif ?>
+
         <?php foreach ($categorys as $key => $value): ?>
             <?php $category = $value['category']; ?>
             <div>
@@ -30,4 +44,5 @@
 </body>
 </html>
 
+<script src="./dist/My97DatePicker/WdatePicker.js"></script>
 <script src="./dist/scripts/script.js"></script>
